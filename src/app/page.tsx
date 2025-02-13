19172e4531d74b5f23fb9ec715e22f1aaba21143
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-02-13 23:39:04 +0200

    Add navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import styles from "./page.module.css"
+import Navigation from "../../components/Navigation"
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <header className={styles.header}>
+        <Navigation />
+      </header>
       <main className={styles.main}>
         <h1 className={styles.title}> Welcome to Next.js! </h1>
       </main>
