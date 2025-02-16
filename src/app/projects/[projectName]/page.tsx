98b435532c26951199338f5dc021fd2c599f4f6d
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-02-16 23:16:11 +0200

    Create a dynamic route for the projects

---
+++ src/app/projects/[projectName]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Project: React.FC = () => {
+  return (
+    <div>
+      <h1>Project</h1>
+      <p>This is a page for a specific project </p>
+    </div>
+  )
+}
+
+export default Project
