f12eb734a9db9bae19c28156108c78efb18f652b
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-02-22 23:05:12 +0200

    Add store usage and modification in the second place

--- src/app/send-message/page.tsx
+++ src/app/send-message/page.tsx
@@ -1,16 +1,43 @@
-import React from "react"
-import { useThemeStore } from "@/store/themeStore"
+"use client";
+
+import React from "react";
+import { useThemeStore } from "@/store/themeStore";
+import { useCounterStore } from "@/store/counterStore";
+import styles from "./styles.module.scss";
 
 const SendMessagePage: React.FC = () => {
-  const { theme } = useThemeStore.getState()
-  console.log("theme from store in server compnent ", theme)
+  const theme = useThemeStore((state) => state.theme);
+  const setTheme = useThemeStore((state) => state.setTheme);
+
+  const handleChangeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const count = useCounterStore((state) => state.count);
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
   return (
     <div>
       <h1>Send Message</h1>
       <p>This is the send message page.</p>
-      <div>{`The current theme is ${theme}`}</div>
+      <div className={styles["data-modification"]}>
+        <div className={styles.theme}>
+          <div className={styles.theme}>{`The current theme is ${theme}`}</div>
+          <div className={styles.theme__controls}>
+            <button onClick={handleChangeTheme}>Change Theme</button>
+          </div>
+        </div>
+        <div className={styles.counter}>
+          <div>{`The current count is ${count}`}</div>
+          <div className={styles.counter__controls}>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SendMessagePage
+export default SendMessagePage;
