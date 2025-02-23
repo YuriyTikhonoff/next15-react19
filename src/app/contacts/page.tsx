58222d787b61e0415c471995fc9022c88de638f8
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-02-23 22:56:56 +0200

    Add fetching data

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,12 +1,23 @@
-import React from "react"
+import { Post } from "@/types/app";
+import React from "react";
 
-const ContactsPage: React.FC = () => {
+const ContactsPage: React.FC = async () => {
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts: Post[] = await data.json();
+  console.log(posts);
   return (
     <div>
       <h1>Contacts</h1>
       <p>This is a contacts page </p>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id} style={{}}>
+            <div>{`${post.author} - ${post.title}`}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactsPage
+export default ContactsPage;
