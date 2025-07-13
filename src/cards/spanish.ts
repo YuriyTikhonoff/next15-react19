07cdc9694c5ebb03e3402b29d5785a58bdb22e33
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-07-13 23:08:47 +0300

    Add card for seeing

--- src/cards/spanish.ts
+++ src/cards/spanish.ts
@@ -188,4 +188,15 @@ export const spanishCards: MemoCard[] = [
     createdAtTimestamp: "",
     useReversedDefaultView: false,
   },
+  {
+    id: "18",
+    title: "Ver",
+    front: "Ver Yo veo Tú ves",
+    back: "To see, I see, you see.",
+    category: "Español",
+    level: 0,
+    lastPracticeTimestamp: "",
+    createdAtTimestamp: "",
+    useReversedDefaultView: false,
+  },
 ]
