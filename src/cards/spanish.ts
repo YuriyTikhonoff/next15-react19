f44927f83d4499017a5be37abe93896924724cda
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-08-04 23:12:09 +0300

    Add card for plane

--- src/cards/spanish.ts
+++ src/cards/spanish.ts
@@ -331,4 +331,15 @@ export const spanishCards: MemoCard[] = [
     createdAtTimestamp: "",
     useReversedDefaultView: false,
   },
+  {
+    id: "32",
+    title: "Avión",
+    front: "Avión",
+    back: "Airplane",
+    category: "Español",
+    level: 0,
+    lastPracticeTimestamp: "",
+    createdAtTimestamp: "",
+    useReversedDefaultView: false,
+  },
 ]
