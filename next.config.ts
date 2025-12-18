c498f01735b550b9a0fd620cc1387087ce36f669
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-12-18 23:36:14 +0200

    Add comment as documentation

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
   reactStrictMode: true,
+  // API proxying to handle CORS and simplify client requests
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `http://${process.env.API_BASE_URL}/:path*`, // backend
+        destination: `http://${process.env.API_BASE_URL}/:path*`, // Proxy to Backend
       },
     ]
   },
