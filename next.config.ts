71f3f9295c708faf43558edc8435c8190396ef68
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2025-12-19 23:22:51 +0200

    Remove protocol variable in proxy middleware

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   // API proxying to handle CORS and simplify client requests
   async rewrites() {
-    const apiProtocol =
-      process.env.API_PROTOCOL ||
-      (process.env.NODE_ENV === "production" ? "https" : "http")
-
     return [
       {
         source: "/api/:path*",
-        destination: `${apiProtocol}://${process.env.API_BASE_URL}/:path*`, // Proxy to Backend
+        destination: `${process.env.API_BASE_URL}/:path*`, // Proxy to Backend
       },
     ]
   },
