1b84ee249fc43d9840be40002ea2e6f03b4393a1
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2026-01-16 22:33:44 +0200

    Add categories to navigation

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -4,6 +4,7 @@ export enum ROUTE_KEYS {
   HOME = "HOME",
   PROJECTS = "PROJECTS",
   MEMO_CARDS = "MEMO_CARDS",
+  CATEGORIES = "CATEGORIES",
   NOT_FOUND = "NOT_FOUND",
   SEND_MESSAGE = "SEND_MESSAGE",
 }
@@ -28,6 +29,11 @@ export const ROUTES_MAP: Record<ROUTE_KEYS, IRoute> = {
   [ROUTE_KEYS.MEMO_CARDS]: {
     path: "/memo-cards",
     title: "Memo Cards",
+    showInNavigation: true,
+  },
+  [ROUTE_KEYS.CATEGORIES]: {
+    path: "/categories",
+    title: "Categories",
     showInNavigation: true,
   },
   [ROUTE_KEYS.CONTACTS]: {
