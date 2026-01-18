3cf76a74c61ff22c207fb5f263d960ac8ecf6612
Yurii Tykhonov <yuryi.tykhonoff@machinemax.com>
2026-01-18 22:36:46 +0200

    Add fetching categories

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,5 +1,14 @@
-const categoriesPage = () => {
-  return <div>Categories Page</div>
+import { apiBaseUrl } from "@/constants/app"
+
+const categoriesPage = async () => {
+  const data = await fetch(`${apiBaseUrl}/categories`, { cache: "no-store" })
+  const categories = await data.json()
+  return (
+    <div>
+      <h2>Categories Page</h2>
+      <pre>{JSON.stringify(categories, null, 2)}</pre>
+    </div>
+  )
 }
 
 export default categoriesPage
